00b80fb8a6a93e3041fc197ab0edc29255ae4121
HadiHD17 <hadihaidar1723@gmail.com>
2025-07-18 21:50:25 +0300

    did some queries

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Agent;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
+        Agent::preventLazyLoading(! app()->isProduction());
+        Agent::preventSilentlyDiscardingAttributes(! app()->isProduction());
     }
 }
